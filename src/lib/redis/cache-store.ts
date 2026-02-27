17ef6f3ac10c523e5267cb6d8833eadddd3da2f3
Islam Yamor <islamyamorz@gmail.com>
2026-02-27 10:35:05 +0300

    provided example for cache store

--- src/lib/redis/cache-store.ts
+++ src/lib/redis/cache-store.ts
@@ -1,26 +1,11 @@
 import ms from 'ms'
 import { withCache } from '@/lib/redis/with-cache'
-import type { ApiResult } from '@/lib/query-helpers/api-call'
-import { getMainApiClient_server } from '@/lib/main-api-sdk/server-client'
-import type { WhoAmIResponse } from '@/lib/main-api-sdk/modules/auth/auth-responses'
-import { exchangeService } from '@/app/api/[[...slugs]]/exchange/exchange.service'
-import type { ExchangeRateMap } from '@/app/api/[[...slugs]]/exchange/exchange.types'
 
-export const whoami_cache = withCache<ApiResult<WhoAmIResponse>>({
+export const example_cache = withCache({
   cacheTtlMs: ms('30s'),
-  cachePrefix: 'whoami',
-  shouldCache: (result) => result.success,
+  cachePrefix: 'example',
+  shouldCache: (_result) => true,
   callback: async () => {
-    const serverClient = await getMainApiClient_server()
-    return await serverClient.auth.whoami()
-  },
-})
-
-export const exchange_cache = withCache<ExchangeRateMap>({
-  cacheTtlMs: ms('2h'),
-  cachePrefix: 'exchange',
-  shouldCache: () => true,
-  callback: async () => {
-    return await exchangeService.fetchExchangeRatesFromSource()
+    return { name: 'example' }
   },
 })
